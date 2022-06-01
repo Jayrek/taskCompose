fadaa802103b1f8042d3ba1da5520a025c4ebf33
Jayrek <jrektabasa@gmail.com>
2022-06-01 21:53:00 +0800

    -add AuthViewModelTest.kt unit testing -refactor AuthRepositoryImpl.kt

--- app/src/main/java/com/fs/jayrek/taskcompose/model/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/com/fs/jayrek/taskcompose/model/repository/AuthRepositoryImpl.kt
@@ -35,42 +35,63 @@ class AuthRepositoryImpl @Inject constructor(
 
     override suspend fun signUpWithEmail(
         email: String,
-        password: String, fName: String, lName: String
+        password: String
     ): Resource<AuthResult> {
         return withContext(Dispatchers.IO) {
             safeApiCall {
                 try {
-                    val auth =
+                    Resource.Success(
                         _auth.createUserWithEmailAndPassword(email, password)
                             .await()
-                    Resource.Success(auth)
+                    )
                 } catch (e: Exception) {
                     Resource.Error(e.message.toString())
                 }
-
-//                val user = User(fName, lName)
-//                _fireStore.collection(StringConstants.DOCUMENT_USER)
-//                    .document(auth.user!!.uid)
-//                    .set(user)
-//                    .await()
-//                Resource.Success(auth)
             }
         }
     }
+
+    override suspend fun saveUserInfo(
+        uid: String,
+        fName: String,
+        lName: String,
+        email: String
+    ): Boolean {
+        return try {
+            val user = User(fName, lName, email)
+            _fireStore.collection(StringConstants.DOCUMENT_USER).document(uid).set(user)
+                .await()
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    override fun getCurrentUserId(): String? = _auth.currentUser?.uid
 
     override suspend fun getUserInfo(
         uid: String
     ): Resource<DocumentSnapshot> {
         return withContext(Dispatchers.IO) {
             safeApiCall {
-                Resource.Success(
-                    _fireStore.collection(StringConstants.DOCUMENT_USER)
-                        .document(uid)
-                        .get().await()
-                )
+                try {
+                    Resource.Success(
+                        _fireStore.collection(StringConstants.DOCUMENT_USER)
+                            .document(uid)
+                            .get().await()
+                    )
+                } catch (e: Exception) {
+                    Resource.Error(e.message.toString())
+                }
             }
         }
     }
 
-    override fun checkUser(): FirebaseUser? = _auth.currentUser
+    override fun checkUser(): FirebaseUser? {
+        return try {
+            _auth.currentUser
+        } catch (e: Exception) {
+            null
+        }
+    }
 }
--- app/src/test/java/com/fs/jayrek/taskcompose/vmodel/AuthViewModelTest.kt
+++ app/src/test/java/com/fs/jayrek/taskcompose/vmodel/AuthViewModelTest.kt
@@ -2,12 +2,17 @@ package com.fs.jayrek.taskcompose.vmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import com.fs.jayrek.taskcompose.getOrAwaitValue
+import com.fs.jayrek.taskcompose.helper.StringConstants.DOCUMENT_USER
+import com.fs.jayrek.taskcompose.model.model.User
 import com.fs.jayrek.taskcompose.model.repository.AuthRepositoryImpl
 import com.fs.jayrek.taskcompose.model.repository.IAuthRepository
 import com.fs.jayrek.taskcompose.model.repository.Resource
 import com.google.android.gms.tasks.Task
 import com.google.common.truth.Truth.assertThat
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.DocumentReference
 import com.google.firebase.firestore.FirebaseFirestore
 import io.mockk.*
 import jp.co.tristone.akaso.MainCoroutineRule
@@ -35,13 +40,24 @@ class AuthViewModelTest {
     @Mock
     private lateinit var fireStore: FirebaseFirestore
 
+    @Mock
+    private lateinit var collectionReference: CollectionReference
+
+    @Mock
+    private lateinit var documentReference: DocumentReference
+
+    @Mock
+    private lateinit var firebaseUser: FirebaseUser
+
     private lateinit var viewModel: AuthViewModel
     private lateinit var repository: IAuthRepository
 
+    private var uid = "test-uid"
     private var email = "test-email"
     private var password = "test-password"
     private var fName = "test-fName"
     private var lName = "test-lName"
+    private val user = User(fName, lName)
 
     @Before
     fun setUp() {
@@ -74,16 +90,30 @@ class AuthViewModelTest {
         assertThat(loader).isFalse()
     }
 
-
     @Test
     fun `sign up check returns resource success`() = runBlocking {
         `when`(auth.createUserWithEmailAndPassword(email, password)).thenReturn(mockTask())
 
-        viewModel.signUp(email, fName, lName, password)
+        viewModel.signUp(email, password)
 
         val result = viewModel.authStatus.getOrAwaitValue()
         val loader = viewModel.loader.getOrAwaitValue()
         assertThat(result is Resource.Success).isTrue()
+        assertThat(loader).isFalse()
+    }
+
+    @Test
+    fun `save user information to fireStore`() = runBlocking {
+        `when`(auth.currentUser).thenReturn(firebaseUser)
+        `when`(auth.currentUser?.uid.toString()).thenReturn(uid)
+
+        `when`(fireStore.collection(DOCUMENT_USER)).thenReturn(collectionReference)
+        `when`(collectionReference.document(uid)).thenReturn(documentReference)
+        `when`(documentReference.set(user)).thenReturn(mockTask())
+
+        val result = viewModel.saveUserDocument(fName, lName, email)
+        val loader = viewModel.loader.getOrAwaitValue()
+        assertThat(result).isEqualTo(true)
         assertThat(loader).isFalse()
     }
 
@@ -96,12 +126,40 @@ class AuthViewModelTest {
             )
         ).thenReturn(mockTask(Exception()))
 
-        viewModel.signUp(email, fName, lName, password)
+        viewModel.signUp(email, fName)
 
         val result = viewModel.authStatus.getOrAwaitValue()
         val loader = viewModel.loader.getOrAwaitValue()
         assertThat(result is Resource.Error).isTrue()
         assertThat(loader).isFalse()
+    }
+
+    @Test
+    fun `check if has user`() = runBlocking {
+        `when`(auth.currentUser).thenReturn(firebaseUser)
+
+        viewModel.checkUserLogIn()
+        val user = viewModel.user.getOrAwaitValue()
+        val loader = viewModel.loader.getOrAwaitValue()
+        assertThat(user).isNotNull()
+        assertThat(loader).isFalse()
+    }
+
+    @Test
+    fun `get user information`() = runBlocking {
+        `when`(auth.currentUser).thenReturn(firebaseUser)
+        `when`(auth.currentUser?.uid.toString()).thenReturn(uid)
+
+        `when`(fireStore.collection(DOCUMENT_USER)).thenReturn(collectionReference)
+        `when`(collectionReference.document(uid)).thenReturn(documentReference)
+        `when`(documentReference.get()).thenReturn(mockTask())
+
+        viewModel.getUser()
+        val user = viewModel.snapShot.getOrAwaitValue()
+        val loader = viewModel.loader.getOrAwaitValue()
+        assertThat(user is Resource.Success).isTrue()
+        assertThat(loader).isFalse()
+
     }
 
     private inline fun <reified T> mockTask(exception: Exception? = null): Task<T> {
